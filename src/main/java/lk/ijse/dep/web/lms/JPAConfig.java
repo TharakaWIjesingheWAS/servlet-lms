3043082fd8521d2ee5a44787dea6bf2e6cce9a40
Tharaka Wijesinghe <stwijasinghe@gmail.com>
2020-09-14 03:08:07 +0530

    complete Jpa Config

--- src/main/java/lk/ijse/dep/web/lms/JPAConfig.java
+++ src/main/java/lk/ijse/dep/web/lms/JPAConfig.java
@@ -1,13 +1,75 @@
 package lk.ijse.dep.web.lms;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @EnableJpaRepositories("lk.ijse.dep.web.lms.repository")
 @EnableTransactionManagement
 @PropertySource("classpath:application.properties")
 @Configuration
 public class JPAConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public static PersistenceExceptionTranslationPostProcessor persistenceExceptionTranslationPostProcessor(){
+        return persistenceExceptionTranslationPostProcessor();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(DataSource ds){
+        LocalContainerEntityManagerFactoryBean lcemfb = new LocalContainerEntityManagerFactoryBean();
+        lcemfb.setDataSource(ds);
+        lcemfb.setJpaVendorAdapter(JpaVendorAdapter());
+        lcemfb.setJpaProperties(jpaProperties());
+        lcemfb.setPackagesToScan("lk.ijse.dep.web.lms");
+        return lcemfb;
+    }
+
+    private Properties jpaProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.hbm2ddl.auto",environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        return properties;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName(environment.getRequiredProperty("javax.persistence.jdbc.driver"));
+        ds.setUrl(environment.getRequiredProperty("javax.persistence.jdbc.url"));
+        ds.setUsername(environment.getRequiredProperty("javax.persistence.jdbc.user"));
+        ds.setPassword(environment.getRequiredProperty("javax.persistence.jdbc.password"));
+        return ds;
+    }
+    private JpaVendorAdapter JpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setDatabasePlatform(environment.getRequiredProperty("hibernate.dialect"));
+        adapter.setShowSql(environment.getRequiredProperty("hibernate.show_sql",Boolean.class));
+        return adapter;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+        return new JpaTransactionManager(emf);
+    }
 }
